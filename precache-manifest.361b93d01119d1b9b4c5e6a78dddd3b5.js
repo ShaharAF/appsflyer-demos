385b3486197f1329d368299b03e47a1eaa84cc9c
shaharc1990 <shaharc1990@gmail.com>
2019-07-10 16:27:33 +0300

    Updates

---
+++ precache-manifest.361b93d01119d1b9b4c5e6a78dddd3b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d57d8c0ee509071e3444ef8a614881e7",
+    "url": "/appsflyer-demos/index.html"
+  },
+  {
+    "revision": "27f2feb3cbbc5510b36b",
+    "url": "/appsflyer-demos/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/appsflyer-demos/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "27f2feb3cbbc5510b36b",
+    "url": "/appsflyer-demos/static/js/main.c6653487.chunk.js"
+  },
+  {
+    "revision": "bd3bd967438ca2190a0e",
+    "url": "/appsflyer-demos/static/js/runtime~main.8bbc7242.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/appsflyer-demos/static/media/logo.5d5d9eef.svg"
+  }
+]);
